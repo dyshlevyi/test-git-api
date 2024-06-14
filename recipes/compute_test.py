fca6a45046b450133d75f564b55f35e0fd19058a
Sergey <sergey@zoomdata.com>
2024-06-14 17:28:30 +0200

    Update compute_test.py

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -9,7 +9,7 @@ from dataiku import pandasutils as pdu
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
-test_df = ... # Compute a Pandas dataframe to write into test
+# test_df = ... # Compute a Pandas dataframe to write into test
 
 
 # Write recipe outputs
